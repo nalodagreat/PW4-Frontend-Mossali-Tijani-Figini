2e2de79768491e149683e3e85f04d81b25c6b530
Davide <davide.figini@itsincom.it>
2024-11-08 22:55:00 +0100

    creation of homepage, user and admin dashboard and cart

---
+++ src/app/components/BannerMacarons.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/BannerMacarons.module.css";
+
+const Banner = () => {
+  return (
+    <div className={styles.banner}></div>
+  );
+};
+
+export default Banner;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import React from "react";
+import Hero from './components/Hero';
+import Presentazione from "./components/Presentazione";
+import Information from "./components/Informazioni";
+import Delizie from "./components/Delizie";
+import Banner from "./components/BannerMacarons";
 
 export default function Home() {
-  return (
-    <>
-    </>
+  return( 
+  <>
+  <Hero></Hero>
+  <Presentazione></Presentazione>
+  <Information></Information>
+  <Banner></Banner>
+  <Delizie></Delizie>
+  </>
   );
 }
