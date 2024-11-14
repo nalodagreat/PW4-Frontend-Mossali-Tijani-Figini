8aea1ca71cc76eae618583a8a38eb6352af2d63f
nalodagreat <nalothegrat0@gmail.com>
2024-11-14 11:30:52 +0100

    adding animation to the banner in the homepage

--- src/components/home/banner/banner.js
+++ src/components/home/banner/banner.js
@@ -1,19 +1,14 @@
-import React from "react";
-import Image from "next/image";
-import styles from "./banner.module.css";
-import bannerImg from "@/public/images/home/banner/macarons.jpg";
+import React from 'react';
+import bannerImg from '@/public/images/home/banner/macarons.jpg';
+import styles from './banner.module.css';
 
 const Banner = () => {
   return (
-    <div className={styles.banner}>
-      <Image
-        src={bannerImg}
-        alt="Macarons banner"
-        layout="fill" 
-        objectFit="cover" 
-        quality={100} 
-        priority
-      />
+    <div 
+      className={styles.banner}
+      style={{ backgroundImage: `url(${bannerImg.src})` }} // Imposta dinamicamente lo sfondo
+    >
+      {/* Altri contenuti */}
     </div>
   );
 };
